f73586428f73f8c616880ff9f37d0f90d26da830
Maimoon <maimoon.ideal@gmail.com>
2026-03-06 20:46:22 +0500

    feat: Add core utilities, model mixins, pagination, and permissions.

--- backend_v2/backend/core/pagination.py
+++ backend_v2/backend/core/pagination.py
@@ -1,7 +1,5 @@
-# core/pagination.py
 from rest_framework.pagination import PageNumberPagination
 
-class StandardResultsSetPagination(PageNumberPagination):
+class StandardPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+    page_size_query_param = None
