9eac1ff16ced0c5eead225c03eea6a59d2847990
Maimoon <maimoon.ideal@gmail.com>
2026-03-07 21:12:57 +0500

    feat: Implement new projects application with API endpoints for project management, including models, serializers, views, and tests.

---
+++ backend_v3/apps/projects/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import Project, ProjectCategory, TechTag, ProjectImage
+
+class TechTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechTag
+        fields = ['name', 'slug']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectCategory
+        fields = ['name', 'slug']
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ['image']  # image URL automatically serialized
+
+class ProjectListSerializer(serializers.ModelSerializer):
+    tags = TechTagSerializer(many=True, read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
+    thumbnail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Project
+        fields = [
+            'id', 'title', 'slug', 'meta_desc',
+            'tags', 'categories', 'year', 'thumbnail',
+            'featured', 'completion_date', 'featured_image'
+        ]
+
+
+    def get_thumbnail(self, obj):
+        first_image = obj.images.first()
+        return first_image.image.url if first_image else None
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    tags = TechTagSerializer(many=True, read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
+    images = ProjectImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = [
+            'id', 'title', 'slug', 'meta_desc',
+            'tags', 'categories', 'year', 'thumbnail',
+            'overview', 'challenge', 'solution', 'role',
+            'timeline', 'live_url', 'github_url',
+            'featured', 'completion_date', 'featured_image',
+            'images'
+        ]
