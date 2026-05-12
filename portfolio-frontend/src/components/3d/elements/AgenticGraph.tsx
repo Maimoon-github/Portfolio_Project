3c4f59e2dee362dc31d8a05df8dff12302aa3160
Maimoon <maimoon.ideal@gmail.com>
2026-05-13 00:54:48 +0500

    feat: implement client-side wrapper component for 3D AgenticGraph with loading state

--- portfolio-frontend/src/components/3d/elements/AgenticGraph.tsx
+++ portfolio-frontend/src/components/3d/elements/AgenticGraph.tsx
@@ -1,18 +1,14 @@
-// src/components/3d/elements/AgenticGraph.tsx
+// src/components/3d/elements/AgenticGraph.tsx (wrapper)
 'use client';
 
 import dynamic from 'next/dynamic';
-import type { AgenticGraphImplProps } from './AgenticGraphImpl';
 import { SectionSkeleton } from '@/components/ui/SectionSkeleton';
 
-const AgenticGraphImpl = dynamic(
-  () => import('./AgenticGraphImpl').then((mod) => mod.AgenticGraphImpl),
-  {
-    ssr: false,
-    loading: () => <SectionSkeleton />,
-  }
-);
+const AgenticGraphImpl = dynamic(() => import('./AgenticGraphImpl').then(mod => mod.AgenticGraphImpl), {
+  ssr: false,
+  loading: () => <SectionSkeleton />,
+});
 
-export function AgenticGraph(props: AgenticGraphImplProps) {
+export function AgenticGraph(props: any) {
   return <AgenticGraphImpl {...props} />;
 }
