8c87a6bf8fb5069212d41ba7ef3403fad5b24e18
Maimoon <maimoon.ideal@gmail.com>
2026-05-17 01:15:19 +0500

    refactor: move all hook files from app directory to root hooks folder

--- frontend/src/app/hooks/useResume.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-import useSWR from "swr";
-import { getResume } from "../../services/api";
-import { ResumeData } from "../types/api";
-
-export function useResume() {
-  const { data, error } = useSWR<ResumeData>("resume", getResume);
-  return { resume: data, loading: !error && !data, error };
-}
