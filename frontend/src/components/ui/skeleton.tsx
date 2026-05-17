474c48d49087fea12ff37b0f7198aad7c30dda34
Maimoon <maimoon.ideal@gmail.com>
2026-05-17 13:44:31 +0500

    refactor: remove commented-out code and streamline UI component implementations across the frontend

--- frontend/src/components/ui/skeleton.tsx
+++ frontend/src/components/ui/skeleton.tsx
@@ -1,46 +1,4 @@
-// import { cn } from "./utils";
-
-// function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
-//   return (
-//     <div
-//       data-slot="skeleton"
-//       className={cn("bg-accent animate-pulse rounded-md", className)}
-//       {...props}
-//     />
-//   );
-// }
-
-// export { Skeleton };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'use client';
 
 import { cn } from "./utils";
 
